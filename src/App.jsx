74d88d3ad4bd59df2b7f2aedfc57f04a643a24ea
Nathan Richards <nathan.richards@razor.co.uk>
2023-01-31 20:55:19 +0000

    Added search params

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { createRoot } from "react-dom";
-import { Pet } from "./Pet";
+import { createRoot } from "react-dom/client";
+import { SearchParams } from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Bird" animal="Pepper" breed="Cockatiel" />
-      <Pet name="Cat" animal="Doink" breed="Tuxedo" />
+      <SearchParams />
     </div>
   );
 };
