7acbf3bfcd1e7cfde18eea8b0b386fd9667f952e
Nathan Richards <nathan.richards@razor.co.uk>
2023-01-31 21:12:42 +0000

    Added useState

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -10,10 +15,40 @@ const SearchParams = () => {
           Location
           <input
             id="location"
-            onChange={(e) => setLocation(e.target.value)}
             value={location}
+            onChange={(e) => setLocation(e.target.value)}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option>Please select</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal.toUpperCase()}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>Please select</option>
+            {breeds.map((breed) => (
+              <option key={breed}>{breed.toUpperCase()}</option>
+            ))}
+          </select>
         </label>
         <button type="submit">Submit</button>
       </form>
