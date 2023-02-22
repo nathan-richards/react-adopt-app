8d4888b3c90233112f9d54e42e48c20f7db10733
Nathan Richards <nathan.richards@razor.co.uk>
2023-02-22 09:18:37 +0000

    Carousel

--- src/fetchSearch.js
+++ src/fetchSearch.js
@@ -4,8 +4,9 @@ async function fetchSearch({ queryKey }) {
     `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
   );
 
-  if (!res.ok)
+  if (!res.ok) {
     throw new Error(`pet search not okay: ${animal}, ${location}, ${breed}`);
+  }
 
   return res.json();
 }
